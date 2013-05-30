9241eff1ec865b4486f25f47a831850b717f2bb4
Jose-Eduardo Alatorre <jebremont@gmail.com>
2013-05-30 10:28:52 -0400

    Esquema de función

--- 02_Fun_Dec.R
+++ 02_Fun_Dec.R
@@ -7,4 +7,30 @@
 
 # Paso 1. Elaboración de un cuadro resumen.
 
-data.frame(decil=1:10,hogares=rep(NA,10),hogac = rep(NA,10))
+N <- 26000
+n <- 2000
+q <- 10
+
+deciles <- data.frame(decil=1:q,hogares=rep(NA,q),hogac = rep(NA,q))
+
+for(i in 1:q){deciles[i,"hogares"]<-round(N/q)
+  deciles[q,"hogares"] <- N-(q-1)*deciles[1,"hogares"]             
+}
+
+deciles[,"hogac"] <- cumsum(deciles[,"hogares"])
+
+deciles
+
+### Matriz de dummies
+
+dmatrix <- as.data.frame(matrix(0,nrow=n,ncol=q))
+names(dmatrix)<-paste0("d",deciles[,1])
+
+
+### Ordenar la base por la variable y
+
+### Generar suma acumulada de factores de expansión 
+
+### Llenar la matrix usando ifelse
+
+### Llenar cuadro resumen con la variable a resumir
