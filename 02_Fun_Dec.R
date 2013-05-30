4b2c34c8d2cd0d09a5a3c145a92d7575a214352f
Jose-Eduardo Alatorre <jebremont@gmail.com>
2013-05-30 09:41:43 -0400

    Primer intento de construir una función para quintiles

---
+++ 02_Fun_Dec.R
@@ -0,0 +1,10 @@
+### Elaboración de función quintil o decil
+
+# Datos necesarios:
+# n: factor de expansión
+# N: Total de población
+# q: número de divisiones
+
+# Paso 1. Elaboración de un cuadro resumen.
+
+data.frame(decil=1:10,hogares=rep(NA,10),hogac = rep(NA,10))
